fc03a18055c82872c11ba82ec913b94dd2c91f62
Allan Jamil <allanjami_91@hotmail.com>
2020-04-12 19:39:43 +0200

    Cleanup

--- src/Controller.java
+++ src/Controller.java
@@ -5,8 +5,6 @@ import javax.inject.Named;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Named
